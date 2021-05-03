28ec387001ab53c74ba9eca9d5a258315787870c
Davi Freire <fsdavi.dev@gmail.com>
2021-05-02 21:34:10 -0300

    creates folder to routes management and Home page route

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
+import { BrowserRouter } from 'react-router-dom';
+import { Routes } from './routes';
+import './styles/global.scss';
+
 function App() {
   return (
-    <div className="App">
-      Hello World!
-    </div>
+    <BrowserRouter>
+      <Routes />
+    </BrowserRouter>
   );
 }
 
